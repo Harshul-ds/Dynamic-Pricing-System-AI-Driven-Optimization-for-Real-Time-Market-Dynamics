a9cbadeb73e3814d39d106d6a67051c385b33e07
Harshul Tandan <62135944+Harshul-ds@users.noreply.github.com>
2025-01-04 02:25:41 -0700

    Create config.py

---
+++ data_ingestion/config.py
@@ -0,0 +1,7 @@
+KAFKA_CONFIG = {
+    "topic_name": "pricing_data",
+    "bootstrap_servers": "localhost:9092",
+    "group_id": "dynamic_pricing_consumer"
+}
+
+BATCH_FILE_PATH = "/data/batch_sales.csv"
